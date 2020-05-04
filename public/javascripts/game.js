781a2d5d5e9732a2ac2d5f8962707ee2ced5d99b
Criotech <opesiyanbola8991@gmail.com>
2020-05-04 23:19:30 +0100

    added brick layout

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,6 +1,7 @@
 import Paddle from './paddle.js';
 import InputHandler from './input.js';
 import Ball from './ball.js';
+import Brick from './brick.js';
 
 export default class Game {
 
@@ -14,7 +15,13 @@ export default class Game {
         this.ball = new Ball(this);
         this.paddle = new Paddle(this);
 
-        this.gameObjects = [this.ball, this.paddle];
+        this.bricks = [];
+        for(let i=0; i<10; i++) {
+            this.bricks.push(new Brick(this, {x: i*52, y: 30}))
+        }
+        
+
+        this.gameObjects = [this.ball, this.paddle, ...this.bricks];
 
         new InputHandler(this.paddle);
 
