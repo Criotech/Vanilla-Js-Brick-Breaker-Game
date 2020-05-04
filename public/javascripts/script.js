301f85b2a4eae26fbbd3408e55af40230c5b9f1f
Criotech <opesiyanbola8991@gmail.com>
2020-05-04 09:07:04 +0100

    draw ball into canvas

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,6 @@
 import Paddle from './paddle.js';
 import InputHandler from './input.js';
+import Ball from './ball.js';
 
 
 let canvas = document.getElementById("gameScreen");
@@ -11,6 +12,7 @@ const GAME_HEIGHT = 600;
 
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+let ball = new Ball();
 
 new InputHandler(paddle);
 
@@ -23,10 +25,10 @@ function gameLoop(timestamp) {
     lastTime = timestamp
 
     ctx.clearRect(0, 0, 800, 600); 
-
     paddle.update(deltaTime);
-
     paddle.draw(ctx);
+
+    ball.draw(ctx);
 
     requestAnimationFrame(gameLoop);
 }
